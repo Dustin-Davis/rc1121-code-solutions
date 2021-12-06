cac1c4e17ca5fef2e38fe23cf186a7e179661fec
Dustin Davis <drpookydavis@comcast.net>
2021-12-06 10:57:09 -0800

    completed javascript-primitives-and-variables.

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,18 @@
+var fullName = 'Dustin Davis';
+console.log('value of fullName:', fullName);
+var isCool = true;
+console.log('value of isCool:', isCool);
+var totalPets = 3;
+console.log('value of totalPets:', totalPets);
+
+console.log('typeof fullName:', typeof 'Dustin Davis');
+console.log('type isCool:', typeof true);
+console.log('typeof totalPets:', typeof 3);
+
+var unknown = null;
+console.log('value of unknown:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never:', typeof never);
