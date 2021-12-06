7bf90150dd11e57bcd932de03bb4a6013a6a0428
Dustin Davis <drpookydavis@comcast.net>
2021-12-06 12:18:09 -0800

    completed javascript-operators-and-expressions.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 10;
+var height = 5;
+var area = width * height;
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 900;
+var payment = 1000;
+var change = bill - payment;
+console.log('value of change:', change);
+console.log('typrof change:', typeof change);
+
+var quizzes = 85;
+var midterm = 89;
+var final = 92;
+var grade = (quizzes + midterm + final) / 3;
+console.log('value of grade:', grade);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Dustin';
+var lastName = 'Davis';
+var fullName = firstName + ' ' + lastName;
+console.log('value of fullName:', fullName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 15;
+var isAcidic = (pH < 7);
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof isAcidic:', typeof isAcidic);
+
+var headCount = 500;
+var isSparta = (headCount === 300);
+console.log('value of isSparta:', isSparta);
+console.log('typeof isSparta:', typeof isSparta);
+
+var motto = fullName;
+motto += ' ' + 'is a GOAT';
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
