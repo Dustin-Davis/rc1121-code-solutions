a36d342306d9319451bc8bd8ebd5d35971ff0d6a
Dustin Davis <drpookydavis@comcast.net>
2021-12-07 10:51:39 -0800

    completed javascript-data-model.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,38 @@
+var orderHistory = [
+  {
+    bookTitle: 'JavaScript for impatient programmers',
+    author: 'Rauschmayer, Dr. Axel',
+    orderPlaced: 'August 4, 2020',
+    total: '$34.00',
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    delivered: 'Aug 8, 2020'
+  },
+  {
+    bookTitle: 'The Timeless Way of Building',
+    author: 'Alexander, Christopher',
+    orderPlaced: 'July 19, 2020',
+    total: '$44.53',
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984269-1280257',
+    delivered: 'Jul 20, 2020'
+  },
+  {
+    title: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for Wil U, PC.Support Turbo and Vibration Features.No Driver and No Lag- Gamecube Adapter',
+    orderPlaced: 'July 4, 2020',
+    total: '$17.22',
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    delivered: 'Jul 7, 2020'
+  },
+  {
+    title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+    bookTitle: 'The Art of Sql',
+    author: 'Faroult, Stephane',
+    orderPlaced: 'July 3, 2020',
+    total: '$138.93',
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    delivered: 'Jul 5, 2020'
+  }
+];
