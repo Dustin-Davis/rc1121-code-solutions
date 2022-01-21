06390368954c530003f8f9497006cdd9a1013f75
Dustin-Davis <drpookydavis@comcast.net>
2022-01-21 11:29:21 -0800

    fixed string-manipulation-3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -5,11 +5,8 @@ function titleCase(title) {
   var lowerCasedList = ['and', 'or', 'nor', 'but', 'a', 'as', 'at', 'by', 'for', 'in', 'of', 'on', 'per', 'to', 'the'];
   var words = lowerCased.split(' ');
   words[0] = words[0].charAt(0).toUpperCase() + words[0].substring(1);
-  if (words[0].charAt(0) === 'J') {
-    words[0] = 'JavaScript:';
-    words[1] = 'The';
-  }
-  for (var i = 1; i < words.length; i++) {
+
+  for (var i = 0; i < words.length; i++) {
     if (lowerCasedList.indexOf(words[i]) === -1) {
       words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1);
     }
@@ -21,6 +18,10 @@ function titleCase(title) {
       var upperCaseLetter = words[i].charAt(location + 1).toUpperCase();
       var newWord = words[i].substring(0, location + 1) + upperCaseLetter + words[i].substring(location + 2, words[i].length);
       words[i] = newWord;
+    }
+    if (words[i] === 'javascript:') {
+      words[0] = 'JavaScript:';
+      words[1] = 'The';
     }
     if (words[i] === 'Javascript') {
       words[i] = 'JavaScript';
