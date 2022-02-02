27162d6ed6aaa8c4b0ae55f8f077ff4b04cdf880
Dustin-Davis <drpookydavis@comcast.net>
2022-02-02 11:00:29 -0800

    completed react-function-components

--- react-function-components/src/index.jsx
+++ react-function-components/src/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// function CustomButton(props) {
-//   return <button>Click, Me!</button>;
-// }
-
-// ReactDOM.render(
-//   <CustomButton />,
-//   document.getElementById('root')
-// );
-
-class CustomButton extends React.Component {
-  render() {
-    return <button>Click, Me!</button>;
-  }
+function CustomButton(props) {
+  return <button>Click, Me!</button>;
 }
-const element = <CustomButton />;
+
 ReactDOM.render(
-  element,
+  <CustomButton />,
   document.getElementById('root')
 );
+
+// class CustomButton extends React.Component {
+//   render() {
+//     return <button>Click, Me!</button>;
+//   }
+// }
+// const element = <CustomButton />;
+// ReactDOM.render(
+//   element,
+//   document.getElementById('root')
+// );
