281aea7712cab18db806b61419aac24329da9476
Dustin-Davis <drpookydavis@comcast.net>
2022-02-02 12:09:24 -0800

    completed react-props-and-expressions

---
+++ react-props-and-expressions/src/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function CustomButton(props) {
+  return <button>{ props.text }</button>;
+}
+
+ReactDOM.render(
+  <CustomButton />,
+  document.getElementById('root')
+);
+
+const element = (
+  <div>
+    <CustomButton text="I" />
+    <CustomButton text="know" />
+    <CustomButton text="React!" />
+  </div>
+);
+
+ReactDOM.render(
+  element,
+  document.querySelector('#root')
+);
